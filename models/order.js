f46b0e3e8e4c9307cefe8756b2733b5b61fbec87
LULU <l.maroccia@studenti.poliba.it>
2022-06-14 19:36:23 +0200

    WIP orders creation

--- models/order.js
+++ models/order.js
@@ -1,1 +1,17 @@
-//creare modello ordini
+const moongose = require('mongoose');
+
+const orderSchema = moongose.Schema({
+    ProductList:[
+        {
+            type : mongoose.Types.ObjectId,
+            ref : 'Product'
+        }
+    ],
+    data : Date,
+    user :{
+        type : mongoose.Types.ObjectId,
+        ref : 'User'
+    }
+
+})
+module.exports = mongoose.model("Order", orderSchema);
