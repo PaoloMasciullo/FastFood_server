3c927ed5358456a24e6dd7d33716ae13a6122008
Paolo Masciullo <p.masciullo@studenti.poliba.it>
2022-06-29 14:48:23 +0200

    fix some bugs

--- router/ordersRouter.js
+++ router/ordersRouter.js
@@ -4,6 +4,7 @@ const ordersController = require('../controllers/ordersController')
 const ordersRouter = express.Router();
 
 ordersRouter.post('/', ordersController.addOrder);
+
 ordersRouter.get('/', ordersController.getOrders);
 ordersRouter.get('/:id', ordersController.getOrderById);
 ordersRouter.delete('/:id', ordersController.deleteOrder);
