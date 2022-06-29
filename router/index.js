9dbc352df7b7b6fd04eb6b6be2740c92393e3280
Marco Valentini <m.valentini7@studenti.poliba.it>
2022-06-29 14:59:46 +0200

    Auto stash before rebase of "origin/master"

--- router/index.js
+++ router/index.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const ordersRouter = require('./ordersRouter'),
     productsRouter = require('./productsRouter'),
-    usersRouter = require('./usersRouter'),
-    methodOverride = require('method-override');
+    usersRouter = require('./usersRouter')
 const passport = require("passport");
 
 const router = express.Router();
-router.use(methodOverride("_method",{
-    methods: ["POST","GET"]
-})); //serve per gestire eventuali richieste di put e delete
 
-router.use('/api/orders',passport.authenticate('jwt', {session: false}, ordersRouter));
-router.use('/api/products',passport.authenticate('jwt', {session: false}, productsRouter));
-router.use('/api/users',usersRouter);
+/*router.use('/api/orders', passport.authenticate('jwt', {session: false}, ordersRouter));
+router.use('/api/products', passport.authenticate('jwt', {session: false}, productsRouter));*/
+router.use('/api/orders', ordersRouter);
+router.use('/api/products', productsRouter);
+router.use('/api/users', usersRouter);
 
 
 module.exports = router;
